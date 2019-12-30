15d243d02a8d9a2f1fa7b19672f4cddbcb3500e1
g3sporter <52547590+g3sporter@users.noreply.github.com>
2019-12-30 09:54:42 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,28 @@
 #SelfService Demo Main.tf 
+variable "name_d" { 
+    default = "notDefined"
+    }
+
+variable "size" {
+  default = "small"
+}
+locals {
+  size = {
+    small = {"count"="2", "instance"="t2.micro"},
+    medium = {"count"="4", "instance"="t2.medium"},
+    large = {"count"="4", "instance"="t2.large"}
+ }
+}
+
+   output count {
+    	value = "${local.size[var.size].count}"
+    }
+
+
+output "Instance" {
+    	value = "${local.size[var.size].instance}"
+    }
+
+ output "workspace" {
+      vaule = "SNOW ticket ${terraform.workspace}"
+ }   
