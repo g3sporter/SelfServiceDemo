6e1b4a9ec8931fa1573224e423192e8c326e168e
g3sporter <52547590+g3sporter@users.noreply.github.com>
2020-01-03 13:57:29 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,25 +1,47 @@
-#SelfService Demo Main.tf 
-variable "name_d" { 
-    default = "notDefined"
-    }
+#SelfService Demo Main.tf
 
-variable "size" {
+variable "instance_size" {
   default = "small"
 }
-locals {
-  size = {
-    small = {"count"="2", "instance"="t2.micro"},
-    medium = {"count"="4", "instance"="t2.medium"},
-    large = {"count"="4", "instance"="t2.large"}
- }
+variable "instance_count" {
+  default = "5"
+}
+variable "instance_environment" {
+  default = "dev"
 }
 
-   output count {
-    	value = "${local.size[var.size].count}"
-    }
+locals {
+  instance_size = {
+    small  = { "instance_type" = "t2.small", "region" = "us-east-2", "ami" = "ami-0d8f6eb4f641ef691" },
+    medium = { "instance_type" = "t2.medium", "region" = "us-east-2", "ami" = "ami-0d8f6eb4f641ef691" },
+    large  = { "instance_type" = "t2.large", "region" = "us-east-2", "ami" = "ami-0d8f6eb4f641ef691" },
+    xlarge = { "instance_type" = "t2.xlarge", "region" = "us-east-2", "ami" = "ami-0d8f6eb4f641ef691" }
+  }
+}
 
 
-output "Instance" {
-    	value = "${local.size[var.size].instance}"
-    }
 
+output "instance_type" {
+  value = "${local.instance_size[var.instance_size].instance_type}"
+}
+#output "instance_ids" {
+#  value = module.aws_main.aws_instance.my_instance[*].private_ip
+#}
+
+output "region" {
+  value = "${local.instance_size[var.instance_size].region}"
+}
+
+output "ami" {
+  value = "${local.instance_size[var.instance_size].ami}"
+}
+
+module "aws_main" {
+  source               = "./aws"
+  instance_type        = "${local.instance_size[var.instance_size].instance_type}"
+  region               = "${local.instance_size[var.instance_size].region}"
+  ami                  = "${local.instance_size[var.instance_size].ami}"
+  instance_size        = var.instance_size
+  instance_count       = var.instance_count
+  instance_environment = var.instance_environment
+}
